4641b125149cee9a0b4dc9bc6b1894a8ea16ecbc
artirajpoot377 <artirajpoot@377gmail.com>
2023-05-29 21:06:11 +0530

    liveweather

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
+
+
+const App = () => {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const apiKey = 'fbad332562eb60fd54fa95612e246dc4'; 
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        setWeatherData(data);
+        setError(null);
+      } else {
+        setError('Unable to fetch weather data. Please try again.');
+      }
+    } catch (error) {
+      setError('An error occurred while fetching weather data.');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-lg-6">
+          <form onSubmit={handleSubmit}>
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter city"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+              />
+              <button className="btn btn-primary" type="submit">
+                Get Weather
+              </button>
+            </div>
+          </form>
+
+          {error && <p className="text-danger">{error}</p>}
+
+          {weatherData && (
+            <div className="mt-4">
+              <h2 className="mb-3">Weather Information</h2>
+              <p>
+                <strong>City:</strong> {weatherData.name}
+              </p>
+              <p>
+                <strong>Temperature:</strong> {weatherData.main.temp}°C
+              </p>
+              <p>
+                <strong>Condition:</strong> {weatherData.weather[0].main}
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
